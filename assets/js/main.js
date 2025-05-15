e1173b49ef4cd148c3bbb8d67897d6d5a170e6de
VernSleep <akmaljefri91@gmail.com>
2025-05-16 03:54:54 +0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+(function($) {
+
+    $(function() {
+
+        var $window = $(window),
+            $body = $('body'),
+            $banner = $('#banner');
+
+        $body.addClass('is-loading');
+
+        $window.on('load', function() {
+            window.setTimeout(function() {
+                $body.removeClass('is-loading');
+            }, 100);
+        });
+
+        $(".bg-img").each(function() {
+            var post = $(this),
+                bg = post.data('bg');
+            post.css('background-image', 'url(images/' + bg + ')');
+        });
+
+        $(".post").each(function() {
+            var p = $(this),
+                i = p.find('.inner'),
+                m = p.find('.more');
+            m.addClass('scrolly');
+            p.scrollex({
+                top: '40vh',
+                bottom: '40vh',
+                terminate: function() { m.removeClass('current'); i.removeClass('current'); },
+                enter: function() { m.addClass('current'); i.addClass('current'); },
+                leave: function() { m.removeClass('current'); i.removeClass('current'); }
+            });
+        });
+
+        $('.scrolly').scrolly();
+
+    });
+
+})(jQuery);
